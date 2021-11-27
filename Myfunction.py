e88ae9875326e9574151f0e75603759b64ae23a7
dipeshpatidarjeeran <dipeshjeeran@gmail.com>
2021-11-27 21:18:06 +0530

    my function project

---
+++ Myfunction.py
@@ -0,0 +1,21 @@
+#function defination
+def myclass(name="rajesh"):   #name is formal argument
+    print(f"hello {name}  you are a  student")
+    print("hello  "+name+" you are a teacher")
+
+#function calling
+myclass('sajid')  #sajid name is actual argument 
+
+#default argument
+
+myclass()
+ #function defination
+
+def subtraction(x,y):
+    return  x - y  # function is a retur value
+
+result=subtraction(56,43)
+
+print(result)
+print(f"the answer is {result}" )
+
